c3261b532adb790773411ff64e323d52974118bd
Muhammad Hijir Sajad <sajadhijir@gmail.com>
2022-08-10 04:30:05 +0800

    Sajad menambahkan fungsi javascript pada index dan list barang dengan menggunakan fetch dari API eksternal

---
+++ Js/index.js
@@ -0,0 +1,106 @@
+let ModalcontainerSayur = document.getElementById("list-sayur");
+let ModalcontainerBuah = document.getElementById("list-buah");
+
+fetch("https://west-broad-gerbil.glitch.me/sayur")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((sayur) => {
+      ModalcontainerSayur.innerHTML += `
+    <div class="Populer-modal modal fade" id=sayur-${sayur.id} tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="close-modal" data-bs-dismiss="modal"><b>Tutup</b></div>
+            <div class="container">
+              <div class="row justify-content-center">
+                <div class="col-lg-8">
+                  <div class="modal-body">
+                    <!-- Product details-->
+                    <h2 class="text-uppercase">${sayur.name}</h2>
+                    <p class="item-intro text-muted"></p>
+                    <img class="img-fluid d-block mx-auto" src=${sayur.photo} alt=${sayur.name} />
+                    <p></p>
+                    <ul class="list-inline">
+                      <li>
+                        <strong>Jenis:</strong>
+                        ${sayur.jenis}
+                      </li>
+                      <li>
+                        <strong>Harga:</strong>
+                        ${sayur.harga}/Kg
+                      </li>
+                      <li>
+                        <strong>Stok:</strong>
+                        ${sayur.stok} Kg
+                      </li>
+                    </ul>
+
+                    <button class="btn btn-success btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                      <span class="material-icons"> add_shopping_cart </span> <br />
+                      Masukan ke Keranjang</button
+                    ><br /><br />
+                    <button class="btn btn-success btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                      <span class="material-icons"> local_atm </span> <br />
+                      Beli Sekarang
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+    });
+  });
+
+fetch("https://west-broad-gerbil.glitch.me/buah")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((buah) => {
+      ModalcontainerBuah.innerHTML += `
+      <div class="Populer-modal modal fade" id=buah-${buah.id} tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="close-modal" data-bs-dismiss="modal"><b>Tutup</b></div>
+            <div class="container">
+              <div class="row justify-content-center">
+                <div class="col-lg-8">
+                  <div class="modal-body">
+                    <!-- Product details-->
+                    <h2 class="text-uppercase">${buah.name}</h2>
+                    <p class="item-intro text-muted"></p>
+                    <img class="img-fluid d-block mx-auto" src=${buah.photo} alt=${buah.name} />
+                    <p></p>
+                    <ul class="list-inline">
+                      <li>
+                        <strong>Jenis:</strong>
+                        ${buah.jenis}
+                      </li>
+                      <li>
+                        <strong>Harga:</strong>
+                        ${buah.harga}/Kg
+                      </li>
+                      <li>
+                        <strong>Stok:</strong>
+                        ${buah.stok} Kg
+                      </li>
+                    </ul>
+
+                    <button class="btn btn-success btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                      <span class="material-icons"> add_shopping_cart </span> <br />
+                      Masukan ke Keranjang</button
+                    ><br /><br />
+                    <button class="btn btn-success btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                      <span class="material-icons"> local_atm </span> <br />
+                      Beli Sekarang
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      `;
+    });
+  });
---
+++ Js/list-barang.js
@@ -0,0 +1,142 @@
+let containerSayur = document.getElementById("container-sayur");
+let ModalcontainerSayur = document.getElementById("list-sayur");
+let containerBuah = document.getElementById("container-buah");
+let ModalcontainerBuah = document.getElementById("list-buah");
+
+fetch("https://west-broad-gerbil.glitch.me/sayur")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((sayur) => {
+      containerSayur.innerHTML += `
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="Barang-item">
+        <a class="Barang-link" data-bs-toggle="modal" href=#sayur-${sayur.id}>
+        <div class="Barang-hover">
+            <div class="Barang-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+          </div>
+          <img class="img-fluid" src=${sayur.photo} alt=${sayur.name} />
+          </a>
+           <div class="Barang-caption">
+          <div class="Barang-caption-heading">${sayur.name}</div>
+          <div class="Barang-caption-subheading text-muted">${sayur.jenis}</div>
+        </div>
+      </div>
+    </div>
+    `;
+
+      ModalcontainerSayur.innerHTML += `
+    <div class="Populer-modal modal fade" id=sayur-${sayur.id} tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="close-modal" data-bs-dismiss="modal"><b>Tutup</b></div>
+            <div class="container">
+              <div class="row justify-content-center">
+                <div class="col-lg-8">
+                  <div class="modal-body">
+                    <!-- Product details-->
+                    <h2 class="text-uppercase">${sayur.name}</h2>
+                    <p class="item-intro text-muted"></p>
+                    <img class="img-fluid d-block mx-auto" src=${sayur.photo} alt=${sayur.name} />
+                    <p></p>
+                    <ul class="list-inline">
+                      <li>
+                        <strong>Jenis:</strong>
+                        ${sayur.jenis}
+                      </li>
+                      <li>
+                        <strong>Harga:</strong>
+                        ${sayur.harga}/Kg
+                      </li>
+                      <li>
+                        <strong>Stok:</strong>
+                        ${sayur.stok} Kg
+                      </li>
+                    </ul>
+
+                    <button class="btn btn-success btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                      <span class="material-icons"> add_shopping_cart </span> <br />
+                      Masukan ke Keranjang</button
+                    ><br /><br />
+                    <button class="btn btn-success btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                      <span class="material-icons"> local_atm </span> <br />
+                      Beli Sekarang
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+    });
+  });
+
+fetch("https://west-broad-gerbil.glitch.me/buah")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((buah) => {
+      containerBuah.innerHTML += `
+    <div class="col-lg-4 col-sm-6 mb-4">
+    <div class="Barang-item">
+      <a class="Barang-link" data-bs-toggle="modal" href=#buah-${buah.id}>
+      <div class="Barang-hover">
+          <div class="Barang-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+        </div>
+        <img class="img-fluid" src=${buah.photo} alt=${buah.name} />
+        </a>
+         <div class="Barang-caption">
+        <div class="Barang-caption-heading">${buah.name}</div>
+        <div class="Barang-caption-subheading text-muted">${buah.jenis}</div>
+      </div>
+    </div>
+  </div>
+    `;
+
+      ModalcontainerBuah.innerHTML += `
+    <div class="Populer-modal modal fade" id=buah-${buah.id} tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="close-modal" data-bs-dismiss="modal"><b>Tutup</b></div>
+            <div class="container">
+              <div class="row justify-content-center">
+                <div class="col-lg-8">
+                  <div class="modal-body">
+                    <!-- Product details-->
+                    <h2 class="text-uppercase">${buah.name}</h2>
+                    <p class="item-intro text-muted"></p>
+                    <img class="img-fluid d-block mx-auto" src=${buah.photo} alt=${buah.name} />
+                    <p></p>
+                    <ul class="list-inline">
+                      <li>
+                        <strong>Jenis:</strong>
+                        ${buah.jenis}
+                      </li>
+                      <li>
+                        <strong>Harga:</strong>
+                        ${buah.harga}/Kg
+                      </li>
+                      <li>
+                        <strong>Stok:</strong>
+                        ${buah.stok} Kg
+                      </li>
+                    </ul>
+
+                    <button class="btn btn-success btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                      <span class="material-icons"> add_shopping_cart </span> <br />
+                      Masukan ke Keranjang</button
+                    ><br /><br />
+                    <button class="btn btn-success btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                      <span class="material-icons"> local_atm </span> <br />
+                      Beli Sekarang
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      `;
+    });
+  });
