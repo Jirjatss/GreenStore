e10aed0ead7930b2f656b35085bb63807712c10a
Muhammad Hijir Sajad <sajadhijir@gmail.com>
2022-08-12 19:42:26 +0800

    Menambah fungsi tambah dan kurang kuantitas buah dan sayur di keranjang page

--- Js/keranjang-belanja.js
+++ Js/keranjang-belanja.js
@@ -11,10 +11,10 @@ sayur.forEach((sayur) => {
     <img src=${sayur.photo} alt="" style="width:50%">
   </div>
   <div class="col-2 rincianbarang kata nama" style="text-align: center;">
-  ${sayur.name}   
+    ${sayur.name}   
   </div>
   <div class="col-2 rincianbarang kata jenis" style="text-align: center;" id="value">
-   ${sayur.harga}(1)
+   ${sayur.harga} (1)
   </div>
   <div class="col-2 rincianbarang kuantitas" style="text-align: center;">
   <button class="btn btn-link px-2" id="minus"
@@ -24,13 +24,13 @@ sayur.forEach((sayur) => {
 
   <input id="numberPlace" min="1" name="quantity" value="1" type="number" />
 
-  <button class="btn btn-link px-2" id="plus"
+  <button class="btn btn-link px-2" id="plus" value="1"
   onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
   <i class="bi bi-plus"></i>
   </button>
 </div>
   <div class="col-2 rincianbarang kata" style="text-align: center;">
-  <button id="hapusitem" ><span class="material-icons">
+  <button class="hapusitem" ><span class="material-icons">
   delete
   </span>
   </button>
@@ -38,32 +38,29 @@ sayur.forEach((sayur) => {
     </div>
 
     `;
+  // Fungsi tambah kuantitas Sayur
   let plusbutton = document.getElementById("plus");
   let minusbutton = document.getElementById("minus");
+
   let quantity = parseInt(document.getElementById("numberPlace").value);
-  // let isi = document.getElementById("value");
+  quantity++;
 
   plusbutton.addEventListener("click", function (event) {
     event.preventDefault();
-    let i = 1;
-    let harga = document.getElementById("value");
-    let set = sayur.harga * (quantity + 1);
-    harga.innerHTML = `${set} (${quantity + 1})`;
 
-    //   alert("sajad");
+    let harga = document.getElementById("value");
+
+    let set = quantity++ * sayur.harga;
+    harga.innerHTML = `${set} (${quantity - 1})`;
   });
+  // Fungsi kurang kuantitas Sayur
   minusbutton.addEventListener("click", function (event) {
     event.preventDefault();
-
-    let i = 1;
+    quantity--;
     let harga = document.getElementById("value");
-    let set = sayur.harga * quantity;
-    harga.innerHTML = `${set} (${quantity})`;
+    let set = sayur.harga * (quantity - 1);
+    harga.innerHTML = `${set} (${quantity - 1})`;
   });
-  // let hapusButton = document.getElementById("hapusitem");
-  // hapusButton.onclick = function (e) {
-  //   alert("Sudah Terhapus");
-  // };
 });
 
 let keranjangBelanjaBuah = document.getElementById("keranjang-belanjaBuah");
@@ -83,23 +80,24 @@ buah.forEach((buah) => {
   <div class="col-2 rincianbarang kata nama" style="text-align: center;">
   ${buah.name}
   </div>
-  <div class="col-2 rincianbarang kata jenis" style="text-align: center;">
-  ${buah.harga}
+  <div class="col-2 rincianbarang kata jenis" style="text-align: center;" id="nilai">
+  ${buah.harga} (1)
   </div>
   <div class="col-2 rincianbarang kuantitas" style="text-align: center;">
-    <button class="btn btn-link px-2" id="minus"
-    onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-    <i class="bi bi-dash"></i>
-    </button>
+  <button class="btn btn-link px-2" id="kurang"
+  onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+  <i class="bi bi-dash"></i>
+  </button>
 
-    <input id="numberPlace" min="1" name="quantity" value="1" type="number" />
+  <input id="numberBuah" min="1" name="quantity" value="1" type="number" />
 
-    <button class="btn btn-link px-2" id="plus">
-    <i class="bi bi-plus"></i>
-    </button>
+  <button class="btn btn-link px-2" id="tambah"
+  onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+  <i class="bi bi-plus"></i>
+  </button>
   </div>
   <div class="col-2 rincianbarang kata" style="text-align: center;">
-    <button id="hapusitem" ><span class="material-icons">
+    <button class="hapusitem" ><span class="material-icons">
       delete
       </span>
       </button>
@@ -107,6 +105,29 @@ buah.forEach((buah) => {
     </div>
 
     `;
+  // Fungsi tambah kuantitas Buah
+  let plusbutton1 = document.getElementById("tambah");
+  let minusbutton1 = document.getElementById("kurang");
+
+  let quantity1 = parseInt(document.getElementById("numberBuah").value);
+  quantity1++;
+
+  plusbutton1.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    let harga1 = document.getElementById("nilai");
+
+    let set = quantity1++ * buah.harga;
+    harga1.innerHTML = `${set} (${quantity1 - 1})`;
+  });
+  // Fungsi kurang kuantitas buah
+  minusbutton1.addEventListener("click", function (event) {
+    event.preventDefault();
+    quantity1--;
+    let harga1 = document.getElementById("nilai");
+    let set = buah.harga * (quantity1 - 1);
+    harga1.innerHTML = `${set} (${quantity1 - 1})`;
+  });
 });
 
 // const items = JSON.parse(localStorage.getItem("cartsayur"));
@@ -121,3 +142,5 @@ buah.forEach((buah) => {
 // const filtered = items.filter((item) => item.id !== 1);
 // localStorage.setItem("cartsayur", JSON.stringify(filtered));
 // reload();
+// let harga = document.getElementById("value");
+// console.log(harga);
--- Js/keranjang.js
+++ Js/keranjang.js
@@ -1,4 +1,19 @@
-// let plusbutton = document.getElementById("plus");
-// let minusbutton = document.getElementById("minus");
-// let quantity = parseInt(document.getElementById("numberPlace").value);
-// // let isi = document.getElementById("value");
+// // let handles = document.getElementsByClassName("plus");
+
+// // function handler() {}
+// // let harga = document.getElementById("value");
+// // let set = sayur.harga * (quantity + 1);
+// // harga.innerHTML = `${set} (${quantity + 1})`;
+
+// // for (let i = 0; i < handles.length; i++) {
+// //   handles[i].addEventListener("click", handler);
+// // }
+
+// let hapusButton = document.getElementsByClassName("hapus-item");
+
+// function hapus() {
+//   alert("sajad");
+// }
+// for (let i = 0; i < hapusButton.length; i++) {
+//   hapusButton[i].addEventListener("click", hapus());
+// }
